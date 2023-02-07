c6bb43e65943376608562d36200339793ee8ca01
Mlie <emipa606@users.noreply.github.com>
2023-02-07 23:29:06 +0100

    First publish

---
+++ README.md
@@ -0,0 +1,23 @@
+# SaveThingAsImage
+
+![Image](https://i.imgur.com/buuPQel.png)
+
+
+I was creating a preview image for a mod and got frustrated that even with the games texture sources downloaded I could not really capture the specific texture I wanted. Pawns, items and buildings can have dynamic colors and shadows in game compared to the source textures.
+
+So I made this mod, it adds a new debug-action called "Save Thing as image".
+When used you can click on any Thing in game and it will export the graphic as seen in game to a png.
+Hopefully someone more than me can get use of it!
+
+![Image](https://i.imgur.com/PwoNOj4.png)
+
+
+
+-  See if the the error persists if you just have this mod and its requirements active.
+-  If not, try adding your other mods until it happens again.
+-  Post your error-log using https://steamcommunity.com/workshop/filedetails/?id=818773962]HugsLib and command Ctrl+F12
+-  For best support, please use the Discord-channel for error-reporting.
+-  Do not report errors by making a discussion-thread, I get no notification of that.
+-  If you have the solution for a problem, please post it to the GitHub repository.
+
+
---
+++ Source/SaveThingAsImage/DebugSpawning.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace SpawnModContent;
+
+public static class DebugSpawning
+{
+    private static string SavePath;
+
+    [DebugAction("Spawning", "Save Thing as image", actionType = DebugActionType.ToolMap,
+        allowedGameStates = AllowedGameStates.PlayingOnMap)]
+    private static void Save()
+    {
+        SavePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+        if (SavePath.NullOrEmpty())
+        {
+            SavePath = GenFilePaths.SaveDataFolderPath;
+        }
+
+        foreach (var thing in Find.CurrentMap.thingGrid.ThingsAt(UI.MouseCell()).ToList())
+        {
+            SaveThing(thing);
+        }
+    }
+
+    private static void SaveThing(Thing thing)
+    {
+        string saveTo;
+        Mesh bodyMesh;
+        Vector3 meshSize;
+        switch (thing)
+        {
+            case Pawn pawn:
+                saveTo = $"{Path.Combine(SavePath, pawn.NameShortColored)}_{thing.Rotation.ToStringHuman()}.png";
+                bodyMesh = pawn.RaceProps.Humanlike
+                    ? HumanlikeMeshPoolUtility.GetHumanlikeBodySetForPawn(pawn).MeshAt(pawn.Rotation)
+                    : pawn.drawer.renderer.graphics.nakedGraphic.MeshAt(pawn.Rotation);
+
+                meshSize = bodyMesh.bounds.size;
+                break;
+            case Corpse corpse:
+                saveTo =
+                    $"{Path.Combine(SavePath, corpse.InnerPawn.NameShortColored)}_{thing.Rotation.ToStringHuman()}.png";
+                bodyMesh = corpse.InnerPawn.RaceProps.Humanlike
+                    ? HumanlikeMeshPoolUtility.GetHumanlikeBodySetForPawn(corpse.InnerPawn)
+                        .MeshAt(corpse.InnerPawn.Rotation)
+                    : corpse.InnerPawn.drawer.renderer.graphics.nakedGraphic.MeshAt(corpse.InnerPawn.Rotation);
+
+                meshSize = bodyMesh.bounds.size;
+                break;
+            default:
+            {
+                if (thing.Graphic.MatSingle.mainTexture == BaseContent.BadTex)
+                {
+                    Messages.Message($"Found no texture for {thing.def.defName}", MessageTypeDefOf.NegativeEvent,
+                        false);
+                    return;
+                }
+
+                saveTo = $"{Path.Combine(SavePath, thing.LabelShort)}.png";
+                if (thing.Graphic.data?.graphicClass != null)
+                {
+                    if (thing.Graphic.data.graphicClass == typeof(Graphic_Multi))
+                    {
+                        saveTo = $"{Path.Combine(SavePath, thing.LabelShort)}_{thing.Rotation.ToStringHuman()}.png";
+                    }
+
+                    if (thing.def.stackLimit > 1)
+                    {
+                        if (thing.stackCount == 1)
+                        {
+                            saveTo = $"{Path.Combine(SavePath, thing.LabelShort)}.png";
+                        }
+                        else if (thing.stackCount == thing.def.stackLimit)
+                        {
+                            saveTo = $"{Path.Combine(SavePath, thing.LabelShort)}_full_stack.png";
+                        }
+                        else
+                        {
+                            saveTo = $"{Path.Combine(SavePath, thing.LabelShort)}_stack.png";
+                        }
+                    }
+
+                    if (thing.Graphic.data.graphicClass == typeof(Graphic_Random))
+                    {
+                        var num = thing.overrideGraphicIndex ?? thing.thingIDNumber;
+                        var graphicObject = (Graphic_Random)thing.Graphic;
+                        var graphicNumber = num % graphicObject.subGraphics.Length;
+                        saveTo = $"{Path.Combine(SavePath, thing.LabelShort)}_{graphicNumber}.png";
+                    }
+                }
+
+                meshSize = thing.Graphic.MeshAt(thing.Rotation).bounds.size;
+
+                break;
+            }
+        }
+
+        var thingSize = new Vector2(meshSize.x * 256, meshSize.z * 256);
+        var texture = GetThingTexture(thing, thingSize);
+
+
+        var thingTextureAsPng = texture.EncodeToPNG();
+        File.WriteAllBytes(saveTo, thingTextureAsPng);
+        Messages.Message($"{thing.def.defName} saved to {Path.GetFullPath(saveTo)}", MessageTypeDefOf.TaskCompletion,
+            false);
+    }
+
+    private static Texture2D GetThingTexture(Thing thing, Vector2 size)
+    {
+        var renderTexture =
+            RenderTexture.GetTemporary(
+                (int)size.x,
+                (int)size.y,
+                24,
+                RenderTextureFormat.Default,
+                RenderTextureReadWrite.Linear);
+
+        var previous = RenderTexture.active;
+        RenderTexture.active = renderTexture;
+        GL.Clear(true, true, Color.clear);
+
+        Texture texture;
+        if (thing is Pawn or Corpse)
+        {
+            if (thing is not Pawn pawn)
+            {
+                pawn = ((Corpse)thing).InnerPawn;
+            }
+
+            if (pawn.RaceProps.Humanlike)
+            {
+                texture = PortraitsCache.Get(pawn, size, thing.Rotation);
+            }
+            else
+            {
+                var zoomLevel = 400f / ((size.x + size.y) / 2);
+
+                texture = PortraitsCache.Get(pawn, size, thing.Rotation, default, zoomLevel);
+            }
+
+            Graphics.Blit(texture, renderTexture);
+        }
+        else
+        {
+            texture = thing.Graphic.ExtractInnerGraphicFor(thing).MatAt(thing.Rotation).mainTexture;
+            if (thing.Graphic is Graphic_StackCount graphic_StackCount)
+            {
+                texture = graphic_StackCount.SubGraphicForStackCount(thing.stackCount, thing.def).MatSingleFor(thing)
+                    .mainTexture;
+            }
+
+            Graphics.Blit(texture, renderTexture, thing.Graphic.MatAt(thing.Rotation, thing));
+        }
+
+        var image = new Texture2D(renderTexture.width, renderTexture.height);
+        image.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        image.Apply();
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(renderTexture);
+        return image;
+    }
+}
